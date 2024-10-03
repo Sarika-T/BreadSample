44a3e02203f3ecfb7d819c804da90ee268e476e4
Sarika-T <talarisarika22@gmail.com>
2024-10-03 09:29:02 +0530

    changes first

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={""} className="App-logo" alt="logo" />
+        <p>
+          Editing <code>src/App.js</code> and saves to reload 
+        </p>
+          Learn React
+      </header>
+    </div>
+  );
+}
+
+export default App;
